79f3c97edc8e93ccb61f4b57bb1e7b4e87f8a415
Anh Nguyen <anh.tuan.nguyen@terralogic.com>
2018-05-20 21:54:24 +0700

    Changed config

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+import Header from './ios/src/components/header';
+
+const App = () => (
+    <Header />
+);
+
+
 
 AppRegistry.registerComponent('albums', () => App);
