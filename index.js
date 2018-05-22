d58be4eea2bc2f22482b7387e852c33cf2e4a528
Anh Nguyen <anh.tuan.nguyen@terralogic.com>
2018-05-22 17:44:03 +0700

    36. Component level state

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './ios/src/components/header';
+import AlbumList from './ios/src/components/AlbumList';
 
-const App = () => (
-    <Header />
+const App = () => (    
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
 );
-
-
 
 AppRegistry.registerComponent('albums', () => App);
--- ios/src/components/header.js
+++ ios/src/components/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => {
-    const {textStyle, viewStyle} = style;
+const Header = (props) => {
+    const { textStyle, viewStyle } = style;
     return (
         <View style={viewStyle}>
-            <Text style={textStyle}>Movie!</Text>
+            <Text style={textStyle}>{ props.headerText }</Text>
         </View>
     );
 };
@@ -16,9 +16,9 @@ const style = {
         justifyContent: 'center',
         alignItems: 'center',
         height: 60,
-        paddingTop:15,
+        paddingTop: 15,
         shadowColor: '#000',
-        shadowOffset: {width: 0, height: 2},
+        shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.2,
         elevation: 2,
         position: 'relative'
