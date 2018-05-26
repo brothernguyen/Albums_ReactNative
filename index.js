6314bace616c5eec1e5da3dfc4cc82f4eeaa57ef
Anh Nguyen <anh.tuan.nguyen@terralogic.com>
2018-05-26 21:16:36 +0700

    50. Styling of button with UX

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import Header from './ios/src/components/header';
 import AlbumList from './ios/src/components/AlbumList';
 
 const App = () => (    
-    <View>
+    <View style={{ flex: 1 }}>
         <Header headerText={'Albums'} />
         <AlbumList />
     </View>
